1e1eaab66f89997e87683c8712a0a2b884d92901
Dyunin Ivan <56828894+SyLeyaN@users.noreply.github.com>
2025-01-23 18:34:54 +0300

    work

---
+++ DunaConverter.Web/Program.cs
@@ -0,0 +1,25 @@
+using DunaConverter.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
